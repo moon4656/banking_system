0926872e23509f3bea4d3d5adc6eadf7faff117c
moon4656 <moon4656@gmail.com>
2024-08-13 16:32:22 +0900

    banking system 수정

---
+++ make_account.py
@@ -0,0 +1,13 @@
+
+def make_account():
+    print("[계좌 개설]")
+    print("계좌ID:")
+    account = input()
+    print("이  름:")
+    name = int(input())
+    print("입금액:")
+    amount = int(input())
+    
+    
+    
+    
