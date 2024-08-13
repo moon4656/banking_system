ec43dbbbf1cba449e1cd9d6ee5803926342a279e
말랑곰 <leehoje000@gmail.com>
2024-08-13 16:40:34 +0900

    refactor: account내부 함수제거 account()함수 제거

--- account.py
+++ account.py
@@ -1,12 +1,8 @@
 class Account:
-    def __init__(self):
-        self.account_id = 0
-        self.balance = 0
-        self.customer_name = ''
-
     
-    def account(self, account_id, customer_name):
+    def __init__(self, account_id, customer_name):
         self.account_id = account_id
+        self.balance = 0
         self.customer_name = customer_name
 
     def get_account_id(self):
