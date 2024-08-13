9c1d938628eb0f45a0adb39762d9c8c853d29357
moon4656 <moon4656@gmail.com>
2024-08-13 16:50:58 +0900

    기능 추가 수정

--- make_account.py
+++ make_account.py
@@ -1,13 +1,19 @@
 
+from account import Account
+import banking_main
+
 def make_account():
+    
     print("[계좌 개설]")
     print("계좌ID:")
-    account = input()
+    id = input()
     print("이  름:")
     name = int(input())
     print("입금액:")
     amount = int(input())
+
+    account = Account(id, name)
+    account.deposit(amount)
     
-    
-    
+    banking_main.account_list.append(account)
     
