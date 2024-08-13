e955156331a40fa828db31ac7e9352ce9a93b629
moon4656 <moon4656@gmail.com>
2024-08-13 16:16:07 +0900

    banking main init commit

---
+++ banking_main.py
@@ -0,0 +1,31 @@
+
+from show_menu import show_menu
+
+def main():
+    
+    account_list = []
+    num_of_accounts = 0
+    
+    """Main function to run the banking system."""
+    while True:
+        show_menu()
+        choice = menu_input()
+        print()
+
+        if choice == 1:
+            make_account()
+        elif choice == 2:
+            deposit_money()
+        elif choice == 3:
+            withdraw_money()
+        elif choice == 4:
+            show_all_account_information()
+        elif choice == 5:
+            print("프로그램을 종료합니다.")
+            break
+        else:
+            print("Illegal selection..")
+            print()
+
+if __name__ == "__main__":
+    main()	
